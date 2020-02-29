a0cb07500e6fb6140a5f45ac36375a114b6ee29e
Alina <alina@Apples-MacBook-Pro.local>
2020-02-29 03:49:03 +0300

    кнопки через inline.svg и добав к ним событ. click

--- js/game.js
+++ js/game.js
@@ -1,26 +1,55 @@
 'use strict'
 
-//Получаем кнопки изменения направления из Object svg 
+//Получаем кнопки изменения направления из svg 
 //и кладем их в переменные
-// window.onload = {
-  // }
-const gameContainer = document.querySelector('.game_container');
-const objectSvg = document.getElementById('dir-butt-object'); //получаем элемент object
-console.log(objectSvg);
-const svgDocument = objectSvg.contentDocument; //получаем svg элемент внутри object
-console.log(svgDocument);
-const upSvg = svgDocument.getElementById('svg_13'); //получаем нужный элемент внутри svg
+
+const upSvg = document.querySelector('#up');
 console.log(upSvg);
+upSvg.addEventListener('click', mouseDirUp);
 
-upSvg.setAttribute("fill", "black"); 
+const rightSvg = document.querySelector('#right');
+rightSvg.addEventListener('click', mouseDirRight);
 
+const downSvg = document.querySelector('#down');
+downSvg.addEventListener('click', mouseDirDown);
+
+const leftSvg = document.querySelector('#left');
+leftSvg.addEventListener('click', mouseDirLeft);
+
+//когда кликнута svg-стрелка вверх
+function mouseDirUp(EO) {
+  EO = EO || window.event;
+  EO.preventDefault();
+  dir = 'up';
+  console.log('EO.target: ', EO.target)
+}
+//когда кликнута svg-стрелка вправо
+function mouseDirRight(EO) {
+  EO = EO || window.event;
+  EO.preventDefault();
+  dir = 'right';
+  console.log('EO.target: ', EO.target)
+}
+//когда кликнута svg-стрелка вниз
+function mouseDirDown(EO) {
+  EO = EO || window.event;
+  EO.preventDefault();
+  dir = 'down';
+  console.log('EO.target: ', EO.target)
+}
+//когда кликнута svg-стрелка влево
+function mouseDirLeft(EO) {
+  EO = EO || window.event;
+  EO.preventDefault();
+  dir = 'left';
+  console.log('EO.target: ', EO.target)
+}
 
 const backCanvas = document.querySelector('#background_controller');
 const backContext = backCanvas.getContext('2d');
 
 const canvas = document.querySelector('#game_field');
 const context = canvas.getContext('2d');
-
 
 //Размер поля
 const fieldSize = { //параметры должны делиться на размер ячейки без остатка
@@ -95,11 +124,14 @@ snake[0] = {
 
 document.addEventListener('keydown', changeDirection);
 
+
+
 function randomizer (a, b) { //a:нижняя граница диапазона, b:верхняя
   return Math.floor(
     Math.random() * (b - a + 1)
   ) + a;
 }
+
 
 let dir; //Направление движения
 //Ф-ция кладет в переменную напр. движения в завис. от нажатой клавиши
@@ -172,7 +204,7 @@ function drawSnake() {
     context.lineTo(snake[i].x, snake[i].y + cell.quarter * 3);
     context.lineTo(snake[i].x, snake[i].y + cell.quarter);
     context.fill();
-    console.log('нарисован 1 сегмент змейки');
+    // console.log('нарисован 1 сегмент змейки');
   }
 }
 
@@ -199,7 +231,7 @@ function newGame() {
 }
 
 function draw() {
-  console.log('рисование запущено');
+  // console.log('рисование запущено');
   context.clearRect(0, 0, fieldSize.x, fieldSize.y);
 
   //Если происходит ошибка при генерации еды, она ловится и генерация запускается снова.
